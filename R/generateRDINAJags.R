58f0e5bd3811f5beed2977f5704b4beaef045b28
drackham <drackham@gmail.com>
2016-05-10 21:11:33 -0600

    Updated notation to be consistent with DeCarlo 2012 paper rather than RUM model

--- R/generateRDINAJags.R
+++ R/generateRDINAJags.R
@@ -11,48 +11,48 @@ generateRDINAJags <- function(){
   modelString <- paste(
 "model
   {
-    for (j in 1:J) { # respondents
-      for (i in 1:I) { # items
-        y[j,i] ~ dbern(p[j,i])
+    for (i in 1:I) { # examinee
+      for (j in 1:J) { # items
+        y[i,k] ~ dbern(p[i,j])
       }
     }
 
-    for (j in 1:J) {
-      alpha1[j] ~ dbern(sc1)
-      alpha2[j] ~ dbern(sc2)
+    for (i in 1:I) {
+      alpha1[i] ~ dbern(sc1)
+      alpha2[i] ~ dbern(sc2)
 
-      p[j,1] <- ilogit(f[1] + d[1] * (alpha1[j]))
-      p[j,2] <- ilogit(f[2] + d[2] * (alpha1[j]))
-      p[j,3] <- ilogit(f[3] + d[3] * (alpha1[j]))
-      p[j,4] <- ilogit(f[4] + d[4] * (alpha1[j]))
-      p[j,5] <- ilogit(f[5] + d[5] * (alpha1[j]))
-      p[j,6] <- ilogit(f[6] + d[6] * (alpha1[j]))
-      p[j,7] <- ilogit(f[7] + d[7] * (alpha1[j]))
-      p[j,8] <- ilogit(f[8] + d[8] * (alpha1[j]))
-      p[j,9] <- ilogit(f[9] + d[9] * (alpha1[j]))
-      p[j,10] <- ilogit(f[10] + d[10] * (alpha1[j]))
+      p[i,1] <- ilogit(f[1] + d[1] * (alpha1[i]))
+      p[i,2] <- ilogit(f[2] + d[2] * (alpha1[i]))
+      p[i,3] <- ilogit(f[3] + d[3] * (alpha1[i]))
+      p[i,4] <- ilogit(f[4] + d[4] * (alpha1[i]))
+      p[i,5] <- ilogit(f[5] + d[5] * (alpha1[i]))
+      p[i,6] <- ilogit(f[6] + d[6] * (alpha1[i]))
+      p[i,7] <- ilogit(f[7] + d[7] * (alpha1[i]))
+      p[i,8] <- ilogit(f[8] + d[8] * (alpha1[i]))
+      p[i,9] <- ilogit(f[9] + d[9] * (alpha1[i]))
+      p[i,10] <- ilogit(f[10] + d[10] * (alpha1[i]))
 
-      p[j,11] <- ilogit(f[11] + d[11] * (alpha2[j]))
-      p[j,12] <- ilogit(f[12] + d[12] * (alpha2[j]))
-      p[j,13] <- ilogit(f[13] + d[13] * (alpha2[j]))
-      p[j,14] <- ilogit(f[14] + d[14] * (alpha2[j]))
-      p[j,15] <- ilogit(f[15] + d[15] * (alpha2[j]))
-      p[j,16] <- ilogit(f[16] + d[16] * (alpha2[j]))
-      p[j,17] <- ilogit(f[17] + d[17] * (alpha2[j]))
-      p[j,18] <- ilogit(f[18] + d[18] * (alpha2[j]))
-      p[j,19] <- ilogit(f[19] + d[19] * (alpha2[j]))
-      p[j,20] <- ilogit(f[20] + d[20] * (alpha2[j]))
+      p[i,11] <- ilogit(f[11] + d[11] * (alpha2[i]))
+      p[i,12] <- ilogit(f[12] + d[12] * (alpha2[i]))
+      p[j,13] <- ilogit(f[13] + d[13] * (alpha2[i]))
+      p[i,14] <- ilogit(f[14] + d[14] * (alpha2[i]))
+      p[i,15] <- ilogit(f[15] + d[15] * (alpha2[i]))
+      p[i,16] <- ilogit(f[16] + d[16] * (alpha2[i]))
+      p[i,17] <- ilogit(f[17] + d[17] * (alpha2[i]))
+      p[i,18] <- ilogit(f[18] + d[18] * (alpha2[i]))
+      p[i,19] <- ilogit(f[19] + d[19] * (alpha2[i]))
+      p[i,20] <- ilogit(f[20] + d[20] * (alpha2[i]))
 
-      p[j,21] <- ilogit(f[21] + d[21] * (alpha1[j] * alpha2[j]))
-      p[j,22] <- ilogit(f[22] + d[22] * (alpha1[j] * alpha2[j]))
-      p[j,23] <- ilogit(f[23] + d[23] * (alpha1[j] * alpha2[j]))
-      p[j,24] <- ilogit(f[24] + d[24] * (alpha1[j] * alpha2[j]))
-      p[j,25] <- ilogit(f[25] + d[25] * (alpha1[j] * alpha2[j]))
-      p[j,26] <- ilogit(f[26] + d[26] * (alpha1[j] * alpha2[j]))
-      p[j,27] <- ilogit(f[27] + d[27] * (alpha1[j] * alpha2[j]))
-      p[j,28] <- ilogit(f[28] + d[28] * (alpha1[j] * alpha2[j]))
-      p[j,29] <- ilogit(f[29] + d[29] * (alpha1[j] * alpha2[j]))
-      p[j,30] <- ilogit(f[30] + d[30] * (alpha1[j] * alpha2[j]))
+      p[i,21] <- ilogit(f[21] + d[21] * (alpha1[i] * alpha2[i]))
+      p[i,22] <- ilogit(f[22] + d[22] * (alpha1[i] * alpha2[i]))
+      p[i,23] <- ilogit(f[23] + d[23] * (alpha1[i] * alpha2[i]))
+      p[i,24] <- ilogit(f[24] + d[24] * (alpha1[i] * alpha2[i]))
+      p[i,25] <- ilogit(f[25] + d[25] * (alpha1[i] * alpha2[i]))
+      p[i,26] <- ilogit(f[26] + d[26] * (alpha1[i] * alpha2[i]))
+      p[i,27] <- ilogit(f[27] + d[27] * (alpha1[i] * alpha2[i]))
+      p[i,28] <- ilogit(f[28] + d[28] * (alpha1[i] * alpha2[i]))
+      p[i,29] <- ilogit(f[29] + d[29] * (alpha1[i] * alpha2[i]))
+      p[i,30] <- ilogit(f[30] + d[30] * (alpha1[i] * alpha2[i]))
 
     }
 
