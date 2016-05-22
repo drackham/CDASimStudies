b2b52cfea558e4f0b806f0bab64f650296d314b8
drackham <drackham@gmail.com>
2016-05-21 19:54:49 -0600

    Included a new sim wrapper for STAN and R-DINA

--- R/RDINAStan.R
+++ R/RDINAStan.R
@@ -1,8 +1,17 @@
-library('devtools')
-install_github("drackham/CDADataSims", ref="develop")
-library('CDADataSims')
-library('parallel')
-library('rstan')
+#' R-DINA Simulation in STAN
+#'
+#' Fit a R-DINA model to simulated data using STAN
+#'
+#' @param data Dataset to be used
+#' @param q Q-matrix to be sued
+#' @param wd Desired working directory (string)
+#' @param cores Number of cores to use
+#' @param iter Number of iterations
+#' @param chains Number of chains
+#' @author Dave Rackham \email{ddrackham@gmail.com}
+#' @keywords R-DINA STAN
+#' @export
+#'
 
 stanSim <- function(data, q, wd, cores, iter, chains ){
   rstan_options(auto_write = TRUE)
@@ -19,13 +28,11 @@ stanSim <- function(data, q, wd, cores, iter, chains ){
 
   setwd(wd)
 
-  fit <- stan(file='RDINA.stan',
+  stan(file='RDINA.stan',
                       data = list(I=I,J=J,y=y),
                       cores = cores,
                       iter = iter,
                       chains = chains,
                       control = list(max_treedepth = 15)
               )
-
-  save(fit, file="stanFit.R")
 }
