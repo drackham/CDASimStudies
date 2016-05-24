b192b5921fdd5edbf0ce22e38a170c4356b763c8
drackham <drackham@gmail.com>
2016-05-23 20:06:31 -0600

    Added IRT.1PL data set and documentation

---
+++ R/CDADataSims.R
@@ -0,0 +1,8 @@
+#' @docType data
+#' @name IRT.1PL
+#' @title IRT 1PL Data
+#' @usage data(IRT.1PL)
+#' @description This data set represents simulated 1PL model data
+#' @author Dave Rackham May 2016
+#' @format A data list of responses, locations etc.
+NULL
