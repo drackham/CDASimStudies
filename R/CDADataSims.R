c05ead4ac2cb0c94c8128f9dad0fac304f5fe7c9
drackham <drackham@gmail.com>
2016-05-23 22:58:25 -0600

    Fixed issue that was preventing the different IRT data sets to load

--- R/CDADataSims.R
+++ R/CDADataSims.R
@@ -1,25 +1,25 @@
 #' @docType data
-#' @name IRT.1PL_100
+#' @name IRT_1PL.100
 #' @title IRT 1PL 100 Data
-#' @usage data(IRT.1PL_100)
+#' @usage data(IRT_1PL.100)
 #' @description This data set represents simulated 1PL model data for 100 examinees
 #' @author Dave Rackham May 2016
 #' @format A data list of responses, locations etc.
 NULL
 
 #' @docType data
-#' @name IRT.1PL_500
+#' @name IRT_1PL.500
 #' @title IRT 1PL 500 Data
-#' @usage data(IRT.1PL_500)
+#' @usage data(IRT_1PL.500)
 #' @description This data set represents simulated 1PL model data for 500 examinees
 #' @author Dave Rackham May 2016
 #' @format A data list of responses, locations etc.
 NULL
 
 #' @docType data
-#' @name IRT.1PL_1000
+#' @name IRT_1PL.1000
 #' @title IRT 1PL 1000 Data
-#' @usage data(IRT.1PL_1000)
+#' @usage data(IRT_1PL.1000)
 #' @description This data set represents simulated 1PL model data for 1000 examinees
 #' @author Dave Rackham May 2016
 #' @format A data list of responses, locations etc.
