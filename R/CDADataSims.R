c72c3ccba965bdbc0fc4580ec50317522ea87496
drackham <drackham@gmail.com>
2016-05-23 22:21:15 -0600

    Added IRT.1PL 100, 500 and 1000 data sets

--- R/CDADataSims.R
+++ R/CDADataSims.R
@@ -1,8 +1,27 @@
 #' @docType data
-#' @name IRT.1PL
-#' @title IRT 1PL Data
-#' @usage data(IRT.1PL)
-#' @description This data set represents simulated 1PL model data
+#' @name IRT.1PL_100
+#' @title IRT 1PL 100 Data
+#' @usage data(IRT.1PL_100)
+#' @description This data set represents simulated 1PL model data for 100 examinees
 #' @author Dave Rackham May 2016
 #' @format A data list of responses, locations etc.
 NULL
+
+#' @docType data
+#' @name IRT.1PL_500
+#' @title IRT 1PL 500 Data
+#' @usage data(IRT.1PL_500)
+#' @description This data set represents simulated 1PL model data for 500 examinees
+#' @author Dave Rackham May 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name IRT.1PL_1000
+#' @title IRT 1PL 1000 Data
+#' @usage data(IRT.1PL_1000)
+#' @description This data set represents simulated 1PL model data for 1000 examinees
+#' @author Dave Rackham May 2016
+#' @format A data list of responses, locations etc.
+NULL
+
---
+++ R/STAN-IRT-1PL.R
@@ -0,0 +1,53 @@
+#' IRT 1PL  Simulation in STAN
+#'
+#' Fit a 1PL model to simulated data using STAN
+#'
+#' @param data Dataset to be used
+#' @param q Q-matrix to be sued
+#' @param wd Desired working directory (string)
+#' @param cores Number of cores to use
+#' @param iter Number of iterations
+#' @param chains Number of chains
+#' @author Dave Rackham \email{ddrackham@gmail.com}
+#' @keywords IRT 1PL STAN
+#' @export
+#'
+
+STAN_IRT_1PL <- function(data, wd, cores, iter, chains ){
+  setwd(wd)
+
+  # model <- system.file("Models", "IRT-1PL.stan", package="CDASimStudies") # See: https://stat.ethz.ch/pipermail/r-help/2010-August/247748.html
+  model <- "IRT-1PL.stan"
+  data <- data
+
+  I <- data$I # persons
+  J <- data$J # items
+  N <- data$N
+  ii <- data$ii
+  jj <- data$jj
+  y <- data$y
+
+  # Inits
+  thetaInit <- rnorm(I, 0, 1)
+  betaInit <- rnorm(J, 0, 1)
+
+  # function form 2 with an argument named `chain_id`
+  # This is copied from ?stan
+  initf2 <- function(chain_id = 1) {
+    list(theta = thetaInit, beta = betaInit)
+  }
+
+  # generate a list of lists to specify initial values
+  # Copied from ?stan
+  init_ll <- lapply(1:chains, function(id) initf2(chain_id = id))
+
+  rstan::stan(file= model,
+                      data = list(I = I, J = J, N = N, ii=ii, jj = jj, y=y),
+                      cores = cores,
+                      iter = iter,
+                      chains = chains,
+                      init = init_ll,
+                      verbose=TRUE,
+                      control = list(max_treedepth = 15)
+              )
+}
