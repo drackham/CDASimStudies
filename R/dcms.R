e5931eb6f2262bf72ccb9ee973ed167764dc9e31
drackham <drackham@gmail.com>
2016-06-11 14:28:34 -0600

    Updated data sets ti include new low noise, high noise, uninformative

--- R/dcms.R
+++ R/dcms.R
@@ -78,3 +78,111 @@ NULL
 #' @author Dave Rackham May 2016
 #' @format A data list of responses, locations etc.
 NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_Poor.100
+#' @title R-DINA Simple Q Flat 100 Data
+#' @usage data(R_DINA_SimpleQ_Poor.100)
+#' @description This data set represents simulated R-DINA Simple Q with fixed d and f at 0 for 100 examinees
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_Poor.500
+#' @title R-DINA Simple Q Flat 500 Data
+#' @usage data(R_DINA_SimpleQ_Poor.500)
+#' @description This data set represents simulated R-DINA Simple Q with fixed d and f at 0 for 500 examinees
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_Poor.1000
+#' @title R-DINA Simple Q Flat 100 Data
+#' @usage data(R_DINA_SimpleQ_Poor.1000)
+#' @description This data set represents simulated R-DINA Simple Q with fixed d and f at 0 for 1000 examinees
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_LN.50
+#' @title R-DINA Simple Q Low noise 50 Data
+#' @usage data(R_DINA_SimpleQ_LN.50)
+#' @description This data set represents simulated R-DINA Simple Q with low noise
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_LN.200
+#' @title R-DINA Simple Q Low noise 200 Data
+#' @usage data(R_DINA_SimpleQ_LN.200)
+#' @description This data set represents simulated R-DINA Simple Q with low noise
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_LN.1000
+#' @title R-DINA Simple Q Low noise 1000 Data
+#' @usage data(R_DINA_SimpleQ_LN.1000)
+#' @description This data set represents simulated R-DINA Simple Q with low noise
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_HN.50
+#' @title R-DINA Simple Q High noise 50 Data
+#' @usage data(R_DINA_SimpleQ_HN.50)
+#' @description This data set represents simulated R-DINA Simple Q with high noise
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_HN.200
+#' @title R-DINA Simple Q High noise 200 Data
+#' @usage data(R_DINA_SimpleQ_HN.200)
+#' @description This data set represents simulated R-DINA Simple Q with high noise
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_HN.1000
+#' @title R-DINA Simple Q High noise 1000 Data
+#' @usage data(R_DINA_SimpleQ_HN.1000)
+#' @description This data set represents simulated R-DINA Simple Q with high noise
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_UnInform.50
+#' @title R-DINA Simple Q High noise 50 Data
+#' @usage data(R_DINA_SimpleQ_UnInform.50)
+#' @description This data set represents simulated R-DINA Simple Q uninformative
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_UnInform.200
+#' @title R-DINA Simple Q High noise 200 Data
+#' @usage data(R_DINA_SimpleQ_UnInform.200)
+#' @description This data set represents simulated R-DINA Simple Q uninformative
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
+
+#' @docType data
+#' @name R_DINA_SimpleQ_UnInform.1000
+#' @title R-DINA Simple Q High noise 1000 Data
+#' @usage data(R_DINA_SimpleQ_UnInform.1000)
+#' @description This data set represents simulated R-DINA Simple Q uninformative
+#' @author Dave Rackham June 2016
+#' @format A data list of responses, locations etc.
+NULL
